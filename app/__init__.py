71d7904e8bdd69f862cb059070b9a6f34c15180b
mross982 <mross982@gmail.com>
2019-01-20 20:32:29 -0600

    added .flaskenv file to set up env variables automatically

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,11 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
+# line below (and from app inport routes) is all you need for a basic app
 app = Flask(__name__)
+# creates the application object as an instance of class Flask imported from the flask package.
+
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -12,3 +16,11 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
+
+'''
+One aspect that may seem confusing at first is that there are two entities named app. 
+The app package is defined by the app directory and the __init__.py script, and is 
+referenced in the from app import routes statement. The app variable is defined as an 
+instance of class Flask in the __init__.py script, which makes it a member of the app 
+package.
+'''
